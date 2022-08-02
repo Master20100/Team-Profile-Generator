2012432ad11ccf656dee52a203fd8887a8137fd6
Mina Ghaly <minanabil@msn.com>
2022-08-02 18:33:03 +1000

    inquirer basic added

---
+++ index.js
@@ -0,0 +1,30 @@
+const inquirer = require("inquirer");
+// const manager = require()
+function init() {
+
+    //will create a new manager then assign values to 
+inquirer.prompt([{
+
+    //manager’s name, employee ID, email address, and office number
+    //here name will be manager.getRole();
+    name:"manager",
+    message:"please enter the team manager’s name",
+},{
+    name:"email",
+    message:"please enter the email",
+},{
+    name:"ID",
+    message:"please enter the ID",
+},
+{
+    name:"officeNumber",
+    message:"please enter the office number"
+}
+]).then(
+    answers=>console.log(answers)
+)
+
+}
+
+init();
+
